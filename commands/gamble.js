50170f555ebf57f3fd85c69dbf42f203f5e35ca3
Glitch (anjaybot) <none>
2018-11-30 09:38:38 +0000

    🍍🌥 Checkpoint ./commands/gamble.js:2240810/36458

--- commands/gamble.js
+++ commands/gamble.js
@@ -6,23 +6,21 @@ let coins = require("../coins.json");
 
 module.exports.run = async (client, message, args) => {
 
-  let amount = 250;
-
   randomNumber = Math.floor(Math.random() * (3 - 1) + 1);
   console.log(randomNumber);
   if(randomNumber==2){
   let sCoins = coins[message.author.id].coins;
-  if(sCoins < amount) return message.reply("Cash Yang Kamu Miliki Tidak Cukup!, Kamu Harus Mempunyai Minimal 300 Cash 💰");
+  if(sCoins < args[0]) return message.reply("Cash Yang Kamu Miliki Tidak Cukup!, Kamu Harus Mempunyai Minimal 300 Cash 💰");
 
-    message.channel.send(`Kamu Telah Memenangkan Gamble, dan Mendapatkan ${amount} Cash 💰`);
+    message.channel.send(`Kamu Telah Memenangkan Gamble, dan Mendapatkan ${args[0]} Cash 💰`);
     coins[message.author.id] = {
-      coins: sCoins + amount
+      coins: sCoins + parseInt(args[0])
     };
   }else{
-    message.channel.send(`Kamu Telah Kalah Gamble, dan Kehilangan ${amount} Cash 💰`);
+    message.channel.send(`Kamu Telah Kalah Gamble, dan Kehilangan ${args[0]} Cash 💰`);
     let sCoins = coins[message.author.id].coins;
     coins[message.author.id] = {
-      coins: sCoins - amount
+      coins: sCoins - parseInt(args[0])
     };
   }
 
