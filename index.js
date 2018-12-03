ff65fdc7db188d56f4faed2043d6d4a370944c78
Glitch (anjaybot) <none>
2018-12-03 15:02:04 +0000

    🍌🍑 Checkpoint ./index.js:2240810/387

--- index.js
+++ index.js
@@ -140,15 +140,16 @@ bot.on('messageDelete', async (message) => {
 
 
 bot.on("channelCreate", async channel => {
-  const logChan =  awaitchannel.guild.channels.find(c => c.name === 'log');
-	if (!logs) return console.log("Can't find logs channel.");
+  const logChan = await db.fetch(`logChan_${channel.guild.id}`);
+  const chan = await channel.guild.channels.find(chan => chan.name.toLowerCase() === logChan.toLowerCase()  || chan.id === logChan.toLowerCase());
+	if (!chan) return;
 	const cembed = new Discord.RichEmbed()
 		.setTitle("**~Channel Dibuat~**")
 		.setColor("#00ff00")
 		.addField("Tipe Channel:",`**${channel.type} channel**`)
     .addField("Nama Channel:" ,`**${channel}**`)
 		.setTimestamp(new Date());
-	logs.send(cembed)
+	chan.send(cembed)
 });
 
 bot.on("channelDelete", async channel => {
