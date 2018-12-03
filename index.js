2a033d7f122d713799ec015db4dfe53a85772a07
Glitch (anjaybot) <none>
2018-12-03 12:41:52 +0000

    😤🎈 Checkpoint ./index.js:2240810/39

--- index.js
+++ index.js
@@ -214,7 +214,7 @@ bot.on("message", async message => {
   .setDescription(`<@${message.author.id}>`)
   .setColor("#13c10d");
 
-  message.channel.send(coinEmbed)
+  message.channel.send(coinEmbed).then(msg => msg.delete(4000));
   }
 
   let xpAdd = Math.floor(Math.random() * 7) + 8;
