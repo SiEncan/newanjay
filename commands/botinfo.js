f2ab62690842b80e844f9b8c05fac93f3b44c42b
Glitch (anjaybot) <none>
2018-12-04 12:47:58 +0000

    📕🐓 Checkpoint ./commands/botinfo.js:2240810/352 ./commands/play.js:2240810/106

--- commands/botinfo.js
+++ commands/botinfo.js
@@ -2,12 +2,20 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) =>{
 
+  let totalSeconds = (bot.uptime / 1000);
+let hours = Math.floor(totalSeconds / 3600);
+totalSeconds %= 3600;
+let minutes = Math.floor(totalSeconds / 60);
+let seconds = totalSeconds % 60;
+  
+  
   let bicon = bot.user.displayAvatarURL;
     let botembed = new Discord.RichEmbed()
     .setTitle("~Informasi Bot~")
     .setColor("#ff6100")
     .setThumbnail(bicon)
     .addField("Nama Bot", bot.user.username)
+    .addField("Uptime:", `${hours} Jam ${minutes} Menit ${seconds} Detik`)
     .addField("Berada di", `${bot.guilds.size} Server 🌏`)
     .addField("Melayani", `${bot.users.size} User 👥`)
     .addField("Dibuat Oleh", `Skinnymanᵛᵉʳᶦᶠᶦᵉᵈ ✓#5654`)
