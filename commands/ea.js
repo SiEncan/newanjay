5b018cc942ba66101987ddcbc3eec72815da8390
Glitch (anjaybot) <none>
2018-12-05 07:03:32 +0000

    🏠🍯 Checkpoint ./commands/nyebat.js:2240810/1386 ./commands/ea.js:2240810/976 ./commands/sebat.js:2240810/75

---
+++ commands/ea.js
@@ -0,0 +1,43 @@
+exports.run = async (client, message, level) => {
+
+    message.channel.send('**Ngudud Dlu Ea Mamang**').then(async msg => {
+        setTimeout(() => {
+            msg.edit('🚬');
+        }, 750);
+        setTimeout(() => {
+            msg.edit('🚬 ☁ ');
+        }, 1000);
+        setTimeout(() => {
+            msg.edit('🚬 ☁☁ ');
+        }, 1500);
+        setTimeout(() => {
+            msg.edit('🚬 ☁☁☁ ');
+        }, 2000);
+        setTimeout(() => {
+            msg.edit('🚬 ☁☁');
+        }, 2500);
+        setTimeout(() => {
+            msg.edit('🚬 ☁');
+        }, 3000);
+        setTimeout(() => {
+            msg.edit('🚬 ');
+        }, 3500);
+        setTimeout(() => {
+            msg.edit(`**Mantab Ea Mamang!!**`);
+        }, 4000);
+    });
+};
+
+exports.conf = {
+    enabled: true,
+    guildOnly: true,
+    aliases: [],
+    permLevel: "User"
+};
+
+exports.help = {
+    name: "sebat",
+    category: "Fun",
+    description: "Smoke everyday :dab:",
+    usage: "smoke"
+};
