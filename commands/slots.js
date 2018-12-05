ec5c2d0f3f5f6e40d5c54a4eb5c4864b0de2426a
Glitch (anjaybot) <none>
2018-12-05 06:41:26 +0000

    🏀🍇 Checkpoint ./commands/slots.js:2240810/4983

---
+++ commands/slots.js
@@ -0,0 +1,56 @@
+const Discord = require('discord.js');
+let coins = require("../coins.json");
+const fs = require("fs");
+
+exports.run = async (client, message, args) => {
+    let sCoins = coins[message.author.id].coins;
+  if(sCoins < args[0]) return message.reply(`Cash Yang Kamu Miliki Tidak Cukup!, Kamu Harus Mempunyai Minimal ${args[0]} Cash 💰`);
+
+    let slots = ["🍎", "🍌", "🍒", "🍓", "🍈"];
+    let result1 = Math.floor((Math.random() * slots.length));
+    let result2 = Math.floor((Math.random() * slots.length));
+    let result3 = Math.floor((Math.random() * slots.length));
+    let name = message.author.displayName;
+    let aicon = message.author.displayAvatarURL;
+
+    if (slots[result1] === slots[result2] && slots[result3]) {
+        let wEmbed = new Discord.RichEmbed()
+            .setFooter("You Won!", aicon)
+            .setTitle(':slot_machine:Slots:slot_machine:')
+            .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
+            .setColor("#f4e842");
+        message.channel.send(wEmbed);
+      
+      coins[message.author.id] = {
+      coins: sCoins + parseInt(args[0])
+    };
+      
+    } else {
+        let embed = new Discord.RichEmbed()
+            .setFooter('You Lost!', aicon)
+            .setTitle(':slot_machine:Slots:slot_machine:')
+            .addField('Result', slots[result1] + slots[result2] + slots[result3], true)
+            .setColor("#f4e842");
+        message.channel.send(embed);
+      
+      let sCoins = coins[message.author.id].coins;
+      coins[message.author.id] = {
+      coins: sCoins - parseInt(args[0])
+    };
+    }
+
+    fs.writeFile("./coins.json", JSON.stringify(coins), (err) => {
+      if(err) console.log(err)
+    });
+}
+
+
+exports.conf = {
+    aliases: []
+};
+
+exports.help = {
+    name: 'slots',
+    description: 'Slot Machine',
+    usage: 'slots'
+}
