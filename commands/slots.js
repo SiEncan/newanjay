4f77db0c0291ee25281bd8fd717e4b0b44ca46cb
Glitch (anjaybot) <none>
2018-12-07 16:03:56 +0000

    💈🏑 Checkpoint ./commands/slots.js:2240810/1669

--- commands/slots.js
+++ commands/slots.js
@@ -12,14 +12,22 @@ exports.run = async (client, message, args) => {
     let result1 = Math.floor((Math.random() * slots.length));
     let result2 = Math.floor((Math.random() * slots.length));
     let result3 = Math.floor((Math.random() * slots.length));
+    let result4 = Math.floor((Math.random() * slots.length));
+    let result5 = Math.floor((Math.random() * slots.length));
+    let result6 = Math.floor((Math.random() * slots.length));
+    let result7 = Math.floor((Math.random() * slots.length));
+    let result8 = Math.floor((Math.random() * slots.length));
+    let result9 = Math.floor((Math.random() * slots.length));
     let name = message.author.displayName;
     let aicon = message.author.displayAvatarURL;
 
     if (slots[result1] === slots[result2] && slots[result3]) {
         let wEmbed = new Discord.RichEmbed()
-            .setFooter("You Won!", aicon)
+            .setFooter(message.author.username, aicon)
             .setTitle(':slot_machine:Slots:slot_machine:')
-            .addField('Result:', slots[result1] + slots[result2] + slots[result3], true)
+            .addField('----------------', `|${slots[result1]}${slots[result2]}${slots[result3]}|`, true)
+            .addField(`|${slots[result3]}${slots[result4]}${slots[result5]}| <--`, `|${slots[result7]}${slots[result8]}${slots[result9]}|`)
+            .addField(`----------------`, `Kamu Menang :smile:`)
             .setColor("#f4e842");
         message.channel.send(wEmbed);
       
@@ -29,10 +37,11 @@ exports.run = async (client, message, args) => {
       
     } else {
         let embed = new Discord.RichEmbed()
-            .setFooter('You Lost!', aicon)
+            .setFooter(message.author.username, aicon)
             .setTitle(':slot_machine:Slots:slot_machine:')
-            .addField('Result', slots[result1] + slots[result2] + slots[result3], true)
-            .addField(`${slots[result1]}<`, `a`)
+            .addField('----------------', `|${slots[result1]}${slots[result2]}${slots[result3]}|`, true)
+            .addField(`|${slots[result3]}${slots[result4]}${slots[result5]}| <--`, `|${slots[result7]}${slots[result8]}${slots[result9]}|`)
+            .addField(`----------------`, `Kamu Kalah :(`)
             .setColor("#f4e842");
         message.channel.send(embed);
       
