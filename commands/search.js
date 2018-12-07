7f0792bee0633a08b89be478bdf809735edb8611
Glitch (anjaybot) <none>
2018-12-07 13:57:57 +0000

    Rewound to commit 0bb7384415b3857e378508ce3cf9159d7eaa8ec9 + 0 edits

--- commands/search.js
+++ commands/search.js
@@ -1,52 +1,15 @@
 const search = require('yt-search');
-const ytdl = require('ytdl-core');
 
-module.exports.run = async (bot, message, args, ops, info) =>{
+module.exports.run = async (bot, message, args, ops) =>{
 
   search(args.join(' '), function(err, res) {
     if(err) return message.channel.send('`Maaf, ada sesuatu yang salah.`');
 
     let videos = res.videos.slice(0, 5);
-    
-    let info = ytdl.getInfo(args[0]);
-    
-    let data = ops.active.get(message.guild.id) || {};
 
-  if(!data.connection) data.connection = message.member.voiceChannel.join();
-
-  if(!data.queue) data.queue = [];
-  data.guildID = message.guild.id;
-
-  data.queue.push({
-      songTitle: info.title,
-      requester: message.author.tag,
-      url: args[0],
-      announceChannel: message.channel.id,
-      length: info.length_seconds,
-      author: info.author.name,
-      thumbnail: info.thumbnail_url
-  });
-    
-    let convert = (input) => {
-    let h = input >= 3600 ? Math.floor(input / 3600) : 0;
-    input %= 3600;
-    let m = input >= 60 ? Math.floor(input / 60) : 0;
-    let s = input % 60;
-    
-    h = check(h);
-    m = check(m);
-    s = check(s);
-
-    return h + ":" + m + ":" + s;
-}
-
-let check = (input) => {
-    return input < 10 ? "0" + input : input;
-}
-    
     let resp = '';
     for(var i in videos) {
-       resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`${convert(info.length_seconds)}\n`;
+       resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
     };
 
     resp += `\n**Ketik Angka Dari** \`1-${videos.length}\``;
