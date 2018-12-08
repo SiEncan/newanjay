1e08294cc99a3b7143d6166c3fb18766c550d275
Glitch (anjaybot) <none>
2018-12-08 02:05:48 +0000

    🛳🌜 Checkpoint ./commands/search.js:2240810/747

--- commands/search.js
+++ commands/search.js
@@ -2,7 +2,7 @@ const search = require('yt-search');
 
 module.exports.run = async (bot, message, args, ops) =>{
 
-  search(args.join(' '), async function(err, res) {
+  search(args.join(' '), function(err, res) {
     if(err) return message.channel.send('`error.`');
 
     let videos = res.videos.slice(0, 5);
@@ -16,16 +16,8 @@ module.exports.run = async (bot, message, args, ops) =>{
 
     message.channel.send(resp)
     
-    try {
-						var filter = await message.channel.awaitMessages(m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0 && m.author.id === message.author.id, {
-							maxMatches: 1,
-							time: 5000,
-							errors: ['time']
-						});
-        } catch (err) {
-						return message.channel.send('Musik Tidak Terpilih.');
-        }
-          
+    
+    const filter = m => !isNaN(m.content) && m.content < videos.length+1 && m.content > 0 && m.author.id === message.author.id;
     const collector = message.channel.createMessageCollector(filter);
 
     collector.videos = videos;
