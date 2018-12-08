9c4f5e0615a332bcdf1ec60b5c552930445cf35c
Glitch (anjaybot) <none>
2018-12-08 07:31:46 +0000

    🎓🍘 Checkpoint ./commands/search.js:2240810/381

--- commands/search.js
+++ commands/search.js
@@ -6,28 +6,11 @@ module.exports.run = async (bot, message, args, ops) =>{
   search(args.join(' '), async function(err, res) {
     if(err) return message.channel.send('`error.`');
     
-    let convert = (input) => {
-    let h = input >= 3600 ? Math.floor(input / 3600) : 0;
-    input %= 3600;
-    let m = input >= 60 ? Math.floor(input / 60) : 0;
-    let s = input % 60;
-    
-    h = check(h);
-    m = check(m);
-    s = check(s);
-
-    return h + ":" + m + ":" + s;
-}
-
-let check = (input) => {
-    return input < 10 ? "0" + input : input;
-
-}
     
     let videos = res.videos.slice(0, 5);
     let resp = '';
     for(var i in videos) {
-       resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`  **Durasi: [${convert(videos[i].seconds)}]**\n`;
+       resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`  **Durasi: [${videos[i].timestamp}]**\n`;
       
     console.log(videos)
     };
