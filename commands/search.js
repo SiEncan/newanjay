66f45b43018a346c3e193310322c742372657c97
Glitch (anjaybot) <none>
2018-12-08 06:58:46 +0000

    📯🌇 Checkpoint ./commands/search.js:2240810/257

--- commands/search.js
+++ commands/search.js
@@ -1,15 +1,15 @@
 const search = require('yt-search');
+const ytdl = require('ytdl-core');
 
 module.exports.run = async (bot, message, args, ops) =>{
 
-  search(args.join(' '), function(err, res) {
+  search(args.join(' '), async function(err, res) {
     if(err) return message.channel.send('`error.`');
 
     let videos = res.videos.slice(0, 5);
-    
     let resp = '';
     for(var i in videos) {
-       resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\n`;
+       resp += `**[${parseInt(i)+1}]:** \`${videos[i].title}\`\`${videos[i].length_seconds}\`\n`;
     };
 
     resp += `\n**Ketik Angka Dari ** \`1-${videos.length}\``;
