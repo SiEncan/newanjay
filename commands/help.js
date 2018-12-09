a92a2d0d89e3c2db22692c4c724457c74f3fd132
Glitch (anjaybot) <none>
2018-12-09 03:59:20 +0000

    🏹🎲 Checkpoint ./commands/help.js:2240810/480

--- commands/help.js
+++ commands/help.js
@@ -15,19 +15,13 @@ module.exports.run = async (bot, message, args) =>{
 
   let helpembed = new Discord.RichEmbed()
   .setTitle("~List Command~")
-  .setDescription(`Commands:`)
   .setColor("#51c7ff")
   .addField(":gear: Moderator", `\`clear\` \`kick\` \`ban\` \`mute\` \`autorole\` \`logchannel\` \`prefix\` \`welcome\``)
   .addField(":moneybag: Ekonomi", `\`cash\` \`pay\` \`dailycash\``)
   .addField("<:casi:521161385095462913> Gamble", `\`gamble\` \`slots\``)
   .addField("🎶 Music", `\`join\` \`leave\` \`play/p\` \`skip/s\` \`pause\` \`resume\` \`queue\` \`volume\` \`lirik\``)
   .addField("<:18o:521168534026584094> NSFW", `\`ass\` \`hentai\` \`tinytits\` \`gif\``)
-  .addField("botinfo",`Menampilkan Informasi Bot`)
-  .addField("level",`Menampilkan Level Kamu`)
-  .addField("say",`Membuat bot mengetik apa yang kamu katakan`)
-  .addField("serverinfo",`Menampilkan Informasi Server`)
-  .addField("ping",`Menampilkan Ping Kamu`)
-  .addField("ask",`Menjawab Pertanyaan`)
+  .addField("Lainnya", `\`ping\` \`ask\` \`say\` \`level\` \`botinfo\` \`serverinfo\``)
 
   .setTimestamp()
   .setFooter("Anjay Bot", bot.user.avatarURL);
