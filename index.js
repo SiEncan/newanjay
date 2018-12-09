8c0302e7adaa889534c01049adf84562664a5ae0
Glitch (anjaybot) <none>
2018-12-09 05:38:27 +0000

    😂🎬 Checkpoint ./index.js:2240810/4200

--- index.js
+++ index.js
@@ -64,6 +64,22 @@ bot.on("guildMemberAdd", member => {
 	member.addRole(role);
 });
 
+const applyText = (canvas, text) => {
+	const ctx = canvas.getContext('2d');
+
+	// Declare a base size of the font
+	let fontSize = 70;
+
+	do {
+		// Assign the font to the context and decrement it so it can be measured again
+		ctx.font = `${fontSize -= 10}px sans-serif`;
+		// Compare pixel width of the text to the canvas minus the approximate avatar size
+	} while (ctx.measureText(text).width > canvas.width - 300);
+
+	// Return the result to use in the actual canvas
+	return ctx.font;
+};
+
 bot.on('guildMemberAdd', async member => {
 	const welChan = await db.fetch(`welChan_${member.guild.id}`);
 
@@ -79,13 +95,19 @@ bot.on('guildMemberAdd', async member => {
 	ctx.strokeStyle = '#74037b';
 	ctx.strokeRect(0, 0, canvas.width, canvas.height);
 
-	// Pick up the pen
+	// Slightly smaller text placed above the member's display name
+	ctx.font = '28px sans-serif';
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText('Welcome to the server,', canvas.width / 2.5, canvas.height / 3.5);
+
+	// Add an exclamation point here and below
+	ctx.font = applyText(canvas, `${member.displayName}!`);
+	ctx.fillStyle = '#ffffff';
+	ctx.fillText(`${member.displayName}!`, canvas.width / 2.5, canvas.height / 1.8);
+
 	ctx.beginPath();
-	// Start the arc to form a circle
 	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	// Put the pen down
 	ctx.closePath();
-	// Clip off the region you drew on
 	ctx.clip();
 
 	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
