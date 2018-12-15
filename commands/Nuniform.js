da2ba65a167f701e0b0b4c4ffe915f71a5636f04
Glitch (anjaybot) <none>
2018-12-15 15:50:19 +0000

    💾☺️ Checkpoint ./commands/Nuniform:2240810/1213 ./commands/Nuniform.js:2240810/96

---
+++ commands/Nuniform.js
@@ -0,0 +1,37 @@
+const Discord = require("discord.js");
+const randomPuppy = require("random-puppy");
+const request = require("snekfetch");
+
+module.exports.run = async (bot, message, args) => {
+    if (!message.channel.nsfw) {
+        message.reply(`🔞 Command Ini Hanya Dapat DiGunakan Di NSFW Channel!`);
+    } else {
+        let sReddits = ["MilitaryGoneWild", "sexyuniforms"];
+        let s = sReddits[Math.round(Math.random() * (sReddits.length - 1))];
+
+        try {
+            randomPuppy(s)
+            .then(url => {
+                const embed = new Discord.RichEmbed()
+                    .setColor(0xffa500)
+                    .setImage(url)
+                    .setTimestamp()
+                    .setFooter(`Direquest Oleh ${message.author.username}`, message.author.avatarURL);
+                message.channel.send({ embed });
+        })
+          } catch (e) {
+            let nsmbed = new Discord.RichEmbed()
+            .setTitle(`**NSFW** 🔞`)
+            .setDescription(`hello`)
+            .setColor(`#ff0000`)
+            .setTimestamp()
+            .setFooter("Direquest oleh", message.author.username);
+
+              message.channel.send(nsmbed);
+          }
+    }
+}
+
+module.exports.help = {
+    name: "uniform"
+}
