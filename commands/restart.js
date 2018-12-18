cdad351de8e2ae7f1cc8630cb7826a48ce83a138
Glitch (anjaybot) <none>
2018-12-18 15:22:24 +0000

    🏕🐯 Checkpoint ./commands/restart.js:2240810/36

--- commands/restart.js
+++ commands/restart.js
@@ -5,7 +5,7 @@ module.exports.run = async (bot, message, args, ops) =>{
   const embed = new Discord.RichEmbed()
     .setTitle('Done.')
     .setDescription(`Restarted in **${Math.floor(bot.ping)}**ms`);
-  if (message.author.id !== '500457305834389529') return;
+  if (message.author.id !== '520782743412539404') return;
   message.channel.send(embed).then(() => {
   process.exit(1);
 })
