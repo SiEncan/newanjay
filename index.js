67b122f747059c8661e7ce6ccd8c0e094d2a4cd6
Glitch (anjaybot) <none>
2018-12-20 11:11:06 +0000

    🌺📷 Checkpoint ./index.js:2240810/6494

--- index.js
+++ index.js
@@ -14,7 +14,6 @@ setInterval(() => {
 const botconfig = require("./botconfig.json");
 const Discord = require("discord.js");
 const fs = require ("fs");
-const Canvas = require('canvas');
 const snekfetch = require('snekfetch');
 const db = require('quick.db');
 const bot = new Discord.Client({disableEveryone: true});
@@ -62,65 +61,6 @@ bot.on("guildMemberAdd", member => {
 	var role = autorole[member.guild.id].role;
 	if (!role) return;
 	member.addRole(role);
-});
-
-const applyText = (canvas, text) => {
-	const ctx = canvas.getContext('2d');
-
-	// Declare a base size of the font
-	let fontSize = 70;
-
-	do {
-		// Assign the font to the context and decrement it so it can be measured again
-		ctx.font = `${fontSize -= 10}px sans-serif`;
-		// Compare pixel width of the text to the canvas minus the approximate avatar size
-	} while (ctx.measureText(text).width > canvas.width - 300);
-
-	// Return the result to use in the actual canvas
-	return ctx.font;
-};
-
-bot.on('guildMemberAdd', async member => {
-	const welChan = await db.fetch(`welChan_${member.guild.id}`);
-
-  const channel = await member.guild.channels.find(channel => channel.name.toLowerCase() === welChan.toLowerCase()  || channel.id === welChan.toLowerCase());
-    if (!channel) return console.log("Tidak bisa menemukan welcome channel");
-  
-	const canvas = Canvas.createCanvas(700, 250);
-	const ctx = canvas.getContext('2d');
-
-	const background = await Canvas.loadImage('https://i.ibb.co/t3NKYXS/Untitled-1.png');
-ctx.drawImage(background, 0, 0, canvas.width, canvas.height);
-
-	ctx.strokeStyle = '#74037b';
-	ctx.strokeRect(0, 0, canvas.width, canvas.height);
-
-	// Slightly smaller text placed above the member's display name
-	ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText('Selamat Datang Di Server', canvas.width / 2.9, canvas.height / 6.0);
-  
-  ctx.font = '28px sans-serif';
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.guild.name}`, canvas.width / 2.5, canvas.height / 3.0);
-
-	// Add an exclamation point here and below
-	ctx.font = applyText(canvas, `${member.user.tag}!`);
-	ctx.fillStyle = '#ffffff';
-	ctx.fillText(`${member.user.tag}`, canvas.width / 2.8, canvas.height / 1.5);
-
-	ctx.beginPath();
-	ctx.arc(125, 125, 100, 0, Math.PI * 2, true);
-	ctx.closePath();
-	ctx.clip();
-
-	const { body: buffer } = await snekfetch.get(member.user.displayAvatarURL);
-	const avatar = await Canvas.loadImage(buffer);
-	ctx.drawImage(avatar, 25, 25, 200, 200);
-
-	const attachment = new Discord.Attachment(canvas.toBuffer(), 'welcome-image.png');
-
-	channel.send(`Welcome to the server, ${member}!`, attachment);
 });
 
 bot.on("guildMemberRemove", async member => {
